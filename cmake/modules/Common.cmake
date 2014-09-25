aef8520665dc97d386376f6bf020b616acda3d9e
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-09-25 19:44:50 +0200

    dont force

--- cmake/modules/Common.cmake
+++ cmake/modules/Common.cmake
@@ -12,9 +12,9 @@ endmacro(SSVCMake_setDefaultSettings)
 macro(SSVCMake_setDefaultFlags)
 	message("SSVCMake: setting default flags")
 
-	set(CMAKE_CXX_FLAGS "-std=c++1y -Wall -Wextra -Wpedantic -pthread" CACHE STRING "" FORCE)
-	set(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -O3" CACHE STRING "" FORCE)
-	set(CMAKE_CXX_FLAGS_DEBUG "-fno-omit-frame-pointer -g3 -gdwarf-2" CACHE STRING "" FORCE)
+	set(CMAKE_CXX_FLAGS "-std=c++1y -Wall -Wextra -Wpedantic -pthread" CACHE STRING "")
+	set(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -O3" CACHE STRING "")
+	set(CMAKE_CXX_FLAGS_DEBUG "-fno-omit-frame-pointer -g3 -gdwarf-2" CACHE STRING "")
 
 	if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 		set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Og")
