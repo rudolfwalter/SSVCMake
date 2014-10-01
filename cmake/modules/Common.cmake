df4584615e1767761d28ea9d95081d089e5ac0c5
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-10-01 18:45:54 +0200

    init-repo

--- cmake/modules/Common.cmake
+++ cmake/modules/Common.cmake
@@ -1,7 +1,7 @@
 macro(SSVCMake_setDefaultSettings)
 	message("SSVCMake: setting default settings")
 
-	set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type.")
+	set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type." FORCE)
 	set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/modules/;${CMAKE_MODULE_PATH}")
 	set(INC_DIR "include")
 	set(SRC_DIR "src")
@@ -12,9 +12,9 @@ endmacro(SSVCMake_setDefaultSettings)
 macro(SSVCMake_setDefaultFlags)
 	message("SSVCMake: setting default flags")
 
-	set(CMAKE_CXX_FLAGS "-std=c++1y -Wall -Wextra -Wpedantic -pthread" CACHE STRING "")
-	set(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -O3" CACHE STRING "")
-	set(CMAKE_CXX_FLAGS_DEBUG "-fno-omit-frame-pointer -g3 -gdwarf-2" CACHE STRING "")
+	set(CMAKE_CXX_FLAGS "-std=c++1y -Wall -Wextra -Wpedantic -pthread" CACHE STRING "" FORCE)
+	set(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -O3" CACHE STRING "" FORCE)
+	set(CMAKE_CXX_FLAGS_DEBUG "-fno-omit-frame-pointer -g3 -gdwarf-2" CACHE STRING "" FORCE)
 
 	if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 		set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Og")
