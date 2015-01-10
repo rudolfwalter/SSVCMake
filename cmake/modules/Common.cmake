4e3907bef79b08292213761aebebad36ae51a5ff
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-01-10 23:06:58 +0100

    progress

--- cmake/modules/Common.cmake
+++ cmake/modules/Common.cmake
@@ -92,7 +92,7 @@ endmacro()
 macro(SSVCMake_setDefaultGlobs)
 #{
 	message("SSVCMake: setting default globs")	
-	file(GLOB_RECURSE SRC_LIST "${INC_DIR}/*.hpp" "${SRC_DIR}/*.hpp")
+	file(GLOB_RECURSE SRC_LIST "${INC_DIR}/*" "${SRC_DIR}/*")
 #}
 endmacro(SSVCMake_setDefaultGlobs)
 
@@ -176,7 +176,7 @@ macro(SSVCMake_setAndInstallHeaderOnly)
 #{
 	message("SSVCMake: setting up and installing as header-only library")
 
-	add_library(HEADER_ONLY_TARGET STATIC ${SRC_LIST} "${INC_DIR}/dummy.cpp")
+	add_library(HEADER_ONLY_TARGET STATIC ${SRC_LIST})
 	set_target_properties(HEADER_ONLY_TARGET PROPERTIES LINKER_LANGUAGE CXX)
 	install(DIRECTORY ${INC_DIR} DESTINATION .)
 #}
