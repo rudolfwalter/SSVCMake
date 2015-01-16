040f37b2ece78199e904920e11ca2b3b7670c50d
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-01-16 18:37:34 +0100

    progress

--- cmake/modules/Common.cmake
+++ cmake/modules/Common.cmake
@@ -176,6 +176,8 @@ macro(SSVCMake_setAndInstallHeaderOnly)
 #{
 	message("SSVCMake: setting up and installing as header-only library")
 
+
+	set_source_files_properties(${SRC_LIST} PROPERTIES HEADER_FILE_ONLY 1)
 	add_library(HEADER_ONLY_TARGET STATIC ${SRC_LIST})
 	set_target_properties(HEADER_ONLY_TARGET PROPERTIES LINKER_LANGUAGE CXX)
 	install(DIRECTORY ${INC_DIR} DESTINATION .)
